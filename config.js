cfe24fa6b15c0ad2394f69b165e35b2fcc0a705c
Nicholas Baxter <nickbaxter4@gmail.com>
2018-12-13 14:58:59 -0500

    config.js and .gitignore
    updated the config.js file to pull the variables from herocku, and updated the gitignored to include the config file.

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.variables = {
+	client_id :  process.env.client_id, // Your client id
+	client_secret :  process.env.client_secret, // Your secret
+	redirect_uri :  process.env.redirect_uri
+}

0facc58287ba6c308ece9eab01913f553adaf99a
Nicholas Baxter <nickbaxter4@gmail.com>
2018-12-13 15:15:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var config = require('./config.js');
 
-var client_id = process.env.client_id; // Your client id
-var client_secret = process.env.client_secret; // Your secret
-var redirect_uri = process.env.redirect_uri; // Your redirect uri
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
